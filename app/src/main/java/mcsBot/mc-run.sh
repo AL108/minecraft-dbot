bee579e067b6187eaf442f2356ac1475086589a9
Ali <alieberman108@gmail.com>
2021-11-20 19:14:39 +1100

    added world forlder to git ignore

--- app/src/main/java/mcsBot/mc-run.sh
+++ app/src/main/java/mcsBot/mc-run.sh
@@ -1,5 +1,5 @@
 
-java -Xmx1024M -Xms1024M -jar /home/alien/Desktop/mc-server/server.jar gui &
+java -Xmx2048M -Xms2048M -jar /home/alien/Desktop/mc-server/server.jar gui &
 mcpid=$!
 sleep 3600
 kill -KILL $mcpid
